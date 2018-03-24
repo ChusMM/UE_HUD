86fcb280e8f602f6bc1fde8ef0560436ff25397b
chuster <jm.munoz.maz@gmail.com>
2018-03-24 02:34:48 +0100

    first

---
+++ Source/UserInterfaces/UserInterfaces.Build.cs
@@ -0,0 +1,38 @@
+/*********************************************************************
+* Module 1. Programming Foundations
+* Author: David Vallejo Fernández    David.Vallejo@uclm.es
+*         Santiago Sánchez Sobrino   Santiago.Sanchez@uclm.es
+*
+* You can redistribute and/or modify this file under the terms of the
+* GNU General Public License ad published by the Free Software
+* Foundation, either version 3 of the License, or (at your option)
+* and later version. See <http://www.gnu.org/licenses/>.
+*
+* This file is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*********************************************************************/
+
+
+using UnrealBuildTool;
+
+public class UserInterfaces : ModuleRules
+{
+	public UserInterfaces(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
